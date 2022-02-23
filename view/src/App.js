195647d43eb80dda5575eb643a4b912703a720dc
Dileep Reddy <saidileepreddyaella@gmail.com>
2022-02-23 22:54:57 +0530

    Add Box component

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'flexnets'
+import { ExampleComponent, Box } from 'flexnets'
 import 'flexnets/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text="Create React Library Example 😄" />
+      <Box color='blue-500' border={'solid'}>
+        Hello
+      </Box>
+    </>
+  )
 }
 
 export default App
